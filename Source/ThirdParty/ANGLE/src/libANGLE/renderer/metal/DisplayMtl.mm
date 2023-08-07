55ca0f3236a347ebcb6f89735e23be82c5aad4af
osy <osy@turing.llc>
2023-08-07 09:09:06 -0700

    ANGLE/Metal: disable timer queries
    Currently they are incomplete due to lack of support for timestamp queries. This causes issues for VirGL.

--- Source/ThirdParty/ANGLE/src/libANGLE/renderer/metal/DisplayMtl.mm
+++ Source/ThirdParty/ANGLE/src/libANGLE/renderer/metal/DisplayMtl.mm
@@ -1034,8 +1034,8 @@ void DisplayMtl::initializeExtensions() const
 
     mNativeExtensions.occlusionQueryBooleanEXT = true;
 
-    mNativeExtensions.disjointTimerQueryEXT = true;
-    mNativeCaps.queryCounterBitsTimeElapsed = 64;
+    mNativeExtensions.disjointTimerQueryEXT = false;
+    mNativeCaps.queryCounterBitsTimeElapsed = 0;
     mNativeCaps.queryCounterBitsTimestamp   = 0;
 
     mNativeExtensions.textureFilterAnisotropicEXT = true;
